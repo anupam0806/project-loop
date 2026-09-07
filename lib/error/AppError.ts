0a3df46d13aed992f79e14d80da85baf60b0b4ad
Anupam Garai <anupamgarai06@gmail.com>
2026-09-07 18:28:18 +0530

    Consolidate auth utilities: remove duplicated lib files

--- lib/error/AppError.ts
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-  code: string;
-  status: number;
-  constructor(code: string, message: string, status: number) {
-    super(message);
-    this.code = code;
-    this.status = status;
-  }
-}
